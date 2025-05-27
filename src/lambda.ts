6de028d135c4e98c4f51c94ba2582b837c8883b1
Anderson Rujeles <anderson.rugeles0606@gmail.com>
2025-05-27 12:35:03 -0500

    Actualizando origin cors

--- src/lambda.ts
+++ src/lambda.ts
@@ -12,8 +12,16 @@ async function bootstrapServer() {
   if (!cachedServer) {
     const expressApp = express();
 
+    const allowedOrigins = [
+      'https://d1d96urffogc3d.cloudfront.net',
+      'http://localhost:5173'
+    ]; 
+
     expressApp.use((req: any, res: any, next: any) => {
-      res.header('Access-Control-Allow-Origin', 'https://d1d96urffogc3d.cloudfront.net');
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+      }
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
       res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
       if (req.method === 'OPTIONS') {
